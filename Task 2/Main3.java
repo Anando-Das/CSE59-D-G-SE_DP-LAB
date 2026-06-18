7015c99c24307d79dbca2783df06b73ab3fdbcaa
Anando Das <developer.anando@gmail.com>
2026-06-18 16:08:32 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,18 @@
+class Countdown {
+    public void Start() {
+        for (int i = 5; i>= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Go!");
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        
+        Countdown timer  = new Countdown();
+        timer.Start();
+    }
+}
 
