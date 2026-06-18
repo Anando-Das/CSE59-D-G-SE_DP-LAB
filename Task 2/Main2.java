815e8a10c88d6273d0d2fa2281db40ae219dd8ff
Anando Das <developer.anando@gmail.com>
2026-06-18 16:03:36 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,23 @@
+class NumberCheck {
+
+    public void check(int num) {
+        if (num % 2 == 0 && num > 10) {
+            System.out.println("true");
+        }else {
+            System.out.println("true");
+        }
+    }
+
+}
+
+
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        NumberCheck checker = new NumberCheck();
+
+        checker.check(14);
+    }
+}
 
