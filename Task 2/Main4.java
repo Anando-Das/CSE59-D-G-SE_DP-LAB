c09130904fd1e8e00841145b8361983109e94bcd
Anando Das <developer.anando@gmail.com>
2026-06-18 16:18:42 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,25 @@
+class Sensor {
+    String name;
+    int batteryLevel;
+
+    public void charge() {
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
+
+
+public class Main4 {
+    public static void main(String[] args) {
+        
+        Sensor p_sensor = new Sensor();
+
+        p_sensor.name = "P sensor A";
+        p_sensor.batteryLevel = 12;
+        p_sensor.charge();
+
+        System.out.println("New battery level: " + p_sensor.batteryLevel + "%");
+
+    }
+}
 
