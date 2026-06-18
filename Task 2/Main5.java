964001717824bd28423979dfd20b9f6032822e60
Anando Das <developer.anando@gmail.com>
2026-06-18 16:25:19 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,25 @@
+class Printer {
+
+    public void printData(String text) {
+        System.out.println("Print text:" + text);
+    }
+
+    public void printData(int number) {
+        System.out.println("Print numver:" + number);
+    }
+
+}
+
+
+public class Main5 {
+    public static void main(String[] args) {
+        
+        Printer myprinter = new Printer();
+
+        myprinter.printData("Hellow world");
+
+        myprinter.printData("42");
+
+    }
+}
 
