fafe0618d15e3922f00e33a27658afed72d67868
Anando Das <developer.anando@gmail.com>
2026-06-18 16:03:01 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,18 @@
 
+class ContestStats  {
+    int[] scores = {80,85,90};
+
+    void printFirstScore() {
+        System.out.println(scores[0]);
+    }
+}
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        
+        ContestStats  contest = new ContestStats();
+        contest.printFirstScore();
+    }
+
+}
