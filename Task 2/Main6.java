91f898b324a1e1ac643514d42622e6007aaa2dd4
Anando Das <developer.anando@gmail.com>
2026-06-18 16:31:37 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,26 @@
+class Applet {
+    public void launch() {
+        System.out.println("Applet launced");
+    }
+}
+
+class GamApplet extends Applet {
+    public void play() {
+        System.out.println("Playing game");
+    }
+}
+
+
+public class Main6 {
+    public static void main(String[] args) {
+        
+        GamApplet myGame = new GamApplet();
+
+        myGame.launch();
+
+        myGame.play();
+
+
+    }
+}
 
