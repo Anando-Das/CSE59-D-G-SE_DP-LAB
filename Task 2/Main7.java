61428dcbd96430acc7a77546bfd90503ac3fefa8
Anando Das <developer.anando@gmail.com>
2026-06-18 16:37:18 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,20 @@
+interface Clicable {
+
+    void onClick();
+    
+}
+
+class Button implements Clicable {
+    public void onClick() {
+        System.out.println("Button was clicked");
+    }
+}
+
+public class Main7 {
+    public static void main(String[] args) {
+        Button myButton = new Button();
+
+        myButton.onClick();
+    }
+}
 
